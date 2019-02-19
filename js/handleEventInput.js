ff4adf0a6dc2a9ba9cdf6e625b03e5f687d19f1c
Glen Paul Florendo <9373317+glenflorendo@users.noreply.github.com>
2019-02-19 12:07:37 -0800

    Implement style editing Resolves #2

--- js/handleEventInput.js
+++ js/handleEventInput.js
@@ -1,11 +1,16 @@
 /**
- * Update text of .text-element in real-time
+ * Update text and style of .text-element in real-time
  * @param {Event} event
  */
 export default function handleEventInput(event) {
+  const textElement = event.currentTarget.parentNode.querySelector('.text-element');
+
   if (event.target.type === 'text' || event.target.tagName === 'TEXTAREA') {
     const text = event.target.value;
-    const textElement = event.currentTarget.parentNode.querySelector('.text-element');
     textElement.innerText = text;
+  } else {
+    const { value, name: styleName } = event.target;
+    const unit = event.target.dataset.unit || '';
+    textElement.style[styleName] = `${value}${unit}`;
   }
 }
