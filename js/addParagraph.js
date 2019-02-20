f1a12e92716e530f155ed0bcefb90ffd0d868a52
Glen Paul Florendo <9373317+glenflorendo@users.noreply.github.com>
2019-02-20 13:20:23 -0800

    Add new heading functionality Resolves #16

--- js/addParagraph.js
+++
@@ -1,4 +0,0 @@
-export default function addParagraph(event) {
-  const newEditableText = document.createElement('wysiwyg-editable-text');
-  document.querySelector('main').insertBefore(newEditableText, event.target);
-}
